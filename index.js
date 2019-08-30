c56750f5436343c1ec9fb74f9a1b25e8a50590a5
Praveen Karmakar <praveen.karmakar@gmail.com>
2019-08-30 11:58:32 +0530

    1

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ app.get('/about', function(req,res){
     res.render('about');
 })
 
+app.get('/login', function(req,res){
+    res.render('login');
+})
+
 app.get('/contact', function(req,res){
     res.render('contact');
 })
